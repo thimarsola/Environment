fb87f00c685848032c7caf70e90de59ef2233eb2
Marsola <tmarsola94@gmail.com>
2023-01-04 10:39:28 -0300

    fix: fix wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -4,7 +4,7 @@ rm -rf .git
 
 docker-compose up -d
 
-curl -O https://wordpress.org/latest.zip && unzip latest.zip && mv wordpress public && rm latest.zip
+curl -LO https://wordpress.org/latest.zip && unzip latest.zip && mv wordpress public && rm latest.zip
 
 docker-compose exec php-fpm chown -R www-data:www-data /application/public
 
