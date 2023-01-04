9c1596f1357d45beb5926628ef6aae645d16394b
Marsola <tmarsola94@gmail.com>
2023-01-04 11:48:35 -0300

    fix: fix sh files

--- wordpress.sh
+++ wordpress.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 rm -rf .git
 docker-compose up -d;
-curl -LO https://wordpress.org/latest.zip && unzip latest.zip && mv wordpress public && rm latest.zip;
+curl -O https://wordpress.org/latest.zip && \
+    unzip latest.zip && \
+    mv wordpress public && \
+    rm latest.zip;
 docker-compose exec php-fpm chown -R www-data:www-data /application/public;
 sudo chmod -R 777 public/
 git clone git@github.com:thimarsola/theme.git public/wp-content/themes/theme;
-cd public/wp-content/themes/theme
+cd public/wp-content/themes/theme;
 sh ./start.sh
