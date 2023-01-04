4529c7072106daf6ee031ff4a42c7a37a6dd72cc
Marsola <tmarsola94@gmail.com>
2023-01-04 11:35:53 -0300

    fix: fix sh files

--- dev.sh
+++ dev.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-
-clear
-
+#!/bin/sh
 rm -rf .git
-
-docker-compose up -d
-
+docker-compose up -d;
 mkdir public
-
-docker-compose exec php-fpm chown -R www-data:www-data /application/public
-
+docker-compose exec php-fpm chown -R www-data:www-data /application/public;
 sudo chmod -R 777 public/
--- wordpress.sh
+++ wordpress.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
-
+#!/bin/sh
 rm -rf .git
-
-docker-compose up -d
-
-curl -LO https://wordpress.org/latest.zip && unzip latest.zip && mv wordpress public && rm latest.zip
-
-docker-compose exec php-fpm chown -R www-data:www-data /application/public
-
+docker-compose up -d;
+curl -LO https://wordpress.org/latest.zip && unzip latest.zip && mv wordpress public && rm latest.zip;
+docker-compose exec php-fpm chown -R www-data:www-data /application/public;
 sudo chmod -R 777 public/
-
-git clone git@github.com:thimarsola/theme.git public/wp-content/themes/theme
-
+git clone git@github.com:thimarsola/theme.git public/wp-content/themes/theme;
 cd public/wp-content/themes/theme
-
-sh start.sh
+sh ./start.sh
